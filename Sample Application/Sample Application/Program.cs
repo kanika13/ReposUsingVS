ac0935d75a9f8ab7d4d6b0ac9b3917a35db43097
Kanika Sharma <kanshar13@gmail.com>
2021-05-02 23:58:59 +0530

    New project added in New local repos

---
+++ Sample Application/Sample Application/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sample_Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //dummy project to test Git via VS
+        }
+    }
+}
